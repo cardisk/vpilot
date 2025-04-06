46efb9f4ea9ebcd5b098b8a7992742a957eec81c
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2025-04-06 21:45:05 +0200

    Application state from CAN bus

--- src/state.h
+++ src/state.h
@@ -29,17 +29,25 @@ struct VPilot_
     struct
     {
         uint8_t info_message;
-        Errors errors;
+        Errors  errors;
     } logs;
 
     struct
     {
-        float voltage_12V;
+        float   voltage_12V;
         uint8_t soc;
 
         uint8_t bms_voltage;
         uint8_t bms_temperature;
     } battery;
+
+    struct
+    {
+        uint16_t rpm;
+        uint8_t  gear;
+        int16_t  gas;
+        int16_t  tps;
+    } engine;
 
     struct
     {
@@ -52,17 +60,21 @@ struct VPilot_
         uint16_t rearR;
     } speed;
 
-	uint8_t lapTimeForTesting;
-	float oilPressure;
-	uint16_t waterTemperature;
-	uint16_t airTemperature;
-	uint16_t engineRPM;
-	uint8_t gear;
-	float manifoldPressure;
-	uint16_t turboPressure;
-	float fuelPressure;
-	int16_t tps;
-	int16_t gas;
+    struct
+    {
+        float    oil;
+        float    manifold;
+        uint16_t turbo;
+        float    fuel;
+    } pressure;
+
+    struct
+    {
+        uint16_t water;
+        uint16_t air;
+    } temperature;
+
+	uint8_t lap_time_for_testing;
 
 #ifdef CAN_AVAILABLE
     int can_socket;
@@ -143,6 +155,70 @@ int vp_update()
                 array_push(&application_state.logs.errors, e);
             }
             break;
+
+        case 700:
+            application_state.speed.gps = (frame.data[1] << 8) | frame.data[0];
+            break;
+
+        case 701:
+            application_state.lap_time_for_testing = data[0];
+            break;
+
+        case 702:
+            application_state.battery.voltage_12V = (data[1] << 8) | data[0];
+            break;
+
+        case 703:
+            application_state.brake.bias = data[0];
+            break;
+
+        case 800:
+            application_state.speed.frontL = (data[1] << 8) | data[0];
+            application_state.speed.frontR = (data[2] << 8) | data[3];
+            application_state.speed.rearL  = (data[4] << 8) | data[5];
+            application_state.speed.rearR  = (data[6] << 8) | data[7];
+            break;
+
+        case 801:
+            application_state.pressure.oil  = ((float) ((data[1] << 8) | data[0])) / 100.0;
+            application_state.pressure.fuel = ((float) ((data[3] << 8) | data[2])) / 100.0;
+            break;
+
+        case 802:
+            application_state.temperature.water = ((data[1] << 8) | data[0]) / 10;
+            application_state.temperature.air   = ((data[3] << 8) | data[2]) / 10;
+            break;
+
+        case 803:
+            application_state.engine.tps        = ((data[1] << 8) | data[0]) / 10.0;
+            application_state.pressure.turbo    =  (data[3] << 8) | data[2];
+            application_state.pressure.manifold = ((float) ((data[5] << 8) | data[4])) / 1000.0;
+            break;
+
+        case 804:
+            application_state.engine.gear = data[0];
+            break;
+        
+        case 805:
+            application_state.engine.rpm = (data[1] << 8) | data[0];
+            application_state.engine.gas = (data[3] << 8) | data[2];
+            break;
+        
+        case 900:
+            application_state.battery.bms_voltage = data[0];
+            break;
+        
+        case 901:
+            application_state.battery.soc = data[0];
+            break;
+        
+        case 902:
+            application_state.battery.bms_temperature = data[0];
+            break;
+
+        default:
+            ERR("Unhandled CAN frame id: %d", frame.can_id);
+            return VP_UPDATE_FAIL;
     }
 #endif
 
